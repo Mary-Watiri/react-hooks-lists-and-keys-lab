de5b9c3752f36ce0311c38ba3bbec4047858abef
Mary Watiri <watirimary30@gmail.com>
2024-01-30 22:45:16 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  
+    return <nav> {links.map(link => <a href={`#${link}`} key={link}>{link}</a>)}</nav>;
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
 }
 
 export default NavBar;
